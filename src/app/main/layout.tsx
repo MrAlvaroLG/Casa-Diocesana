a6461aea9b4592f4378ed63398c10d3c64499f89
MrAlvaroLG <alvarolg.developer@gmail.com>
2025-04-11 18:57:25 -0400

    feat: simplificar el menú de la guía diocesana utilizando un mapeo dinámico

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
     return (
         <html lang="en">
             <header className="py-2 lg:py-5">
-                <div className="lg:hidden flex flex-row items-center content-center justify-start h-12">
+                <div className="lg:hidden flex items-center justify-start h-12">
                     <button 
                         onClick={toggleMenu} 
                         className="ml-2 focus:outline-none"
@@ -48,10 +48,13 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
                 </div>
                 
                 <Menubar 
-                    className={`lg:ml-10 flex border-none shadow-none 
-                        lg:flex-row lg:static lg:w-auto lg:opacity-100 lg:translate-y-0 lg:pointer-events-auto
-                        ${menuOpen ? 'flex-col absolute w-full z-10 opacity-100 translate-y-0' : 'flex-col absolute w-full z-10 opacity-0 -translate-y-5 pointer-events-none lg:pointer-events-auto'}
-                        transition-all duration-300 ease-in-out`}>
+                    className={`border-none shadow-none transition-all duration-300 ease-in-out
+                        lg:ml-10 lg:flex-row lg:static lg:w-auto lg:opacity-100 lg:translate-y-0 
+                        absolute w-full z-10 flex-col
+                        ${menuOpen 
+                            ? 'opacity-100 translate-y-0 pointer-events-auto' 
+                            : 'opacity-0 -translate-y-5 pointer-events-none lg:pointer-events-auto'
+                        }`}>
                     <MenubarMenu>
                         <MenubarTrigger className="lg:w-auto w-full text-center">
                             <Link href="/" onClick={() => setMenuOpen(false)}>Home</Link>
@@ -66,7 +69,7 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
                             <span>Guía Diocesana</span>
                             <ChevronDown 
                                 className={`mr-3 h-4 w-4 transition-transform duration-300 ${
-                                    openSubmenu === 'guia' ? 'transform rotate-180' : ''
+                                    openSubmenu === 'guia' ? 'rotate-180' : ''
                                 }`} 
                             />
                         </div>
@@ -76,33 +79,17 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
                             }`}
                         >
                             <div className="border-l-2 w-full ml-4 pl-2">
-                                <div>
-                                    <Link 
-                                        href="../main/guia-diocesana/horarios-de-misa" 
-                                        onClick={() => setMenuOpen(false)}
-                                        className="block w-full text-sm py-1.5 font-medium hover:bg-accent/80 hover:text-accent-foreground rounded-sm px-2"
-                                    >
-                                        Horarios de Misa
-                                    </Link>
-                                </div>
-                                <div>
-                                    <Link 
-                                        href="../main/guia-diocesana/sacerdotes" 
-                                        onClick={() => setMenuOpen(false)}
-                                        className="block w-full text-sm py-1.5 font-medium hover:bg-accent/80 hover:text-accent-foreground rounded-sm px-2"
-                                    >
-                                        Sacerdotes
-                                    </Link>
-                                </div>
-                                <div>
-                                    <Link 
-                                        href="../main/guia-diocesana/obispo" 
-                                        onClick={() => setMenuOpen(false)}
-                                        className="block w-full text-sm py-1.5 font-medium hover:bg-accent/80 hover:text-accent-foreground rounded-sm px-2"
-                                    >
-                                        Obispo
-                                    </Link>
-                                </div>
+                                {['horarios-de-misa', 'sacerdotes', 'obispo'].map((item) => (
+                                    <div key={item}>
+                                        <Link 
+                                            href={`../main/guia-diocesana/${item}`}
+                                            onClick={() => setMenuOpen(false)}
+                                            className="block w-full text-sm py-1.5 font-medium hover:bg-accent/80 hover:text-accent-foreground rounded-sm px-2"
+                                        >
+                                            {item.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}
+                                        </Link>
+                                    </div>
+                                ))}
                             </div>
                         </div>
                     </div>
@@ -117,15 +104,13 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
                                 <ChevronDown className="ml-1 h-4 w-4 transition-transform duration-300 group-data-[state=open]:rotate-180" />
                             </MenubarTrigger>
                             <MenubarContent className="animate-in fade-in-50 zoom-in-95 duration-200">
-                                <MenubarItem>   
-                                    <Link href="../main/guia-diocesana/horarios-de-misa">Horarios de Misa</Link>
-                                </MenubarItem>
-                                <MenubarItem>   
-                                    <Link href="../main/guia-diocesana/sacerdotes">Sacerdotes</Link>
-                                </MenubarItem>
-                                <MenubarItem>   
-                                    <Link href="../main/guia-diocesana/obispo">Obispo</Link>
-                                </MenubarItem>
+                                {['horarios-de-misa', 'sacerdotes', 'obispo'].map((item) => (
+                                    <MenubarItem key={item}>   
+                                        <Link href={`../main/guia-diocesana/${item}`}>
+                                            {item.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')}
+                                        </Link>
+                                    </MenubarItem>
+                                ))}
                             </MenubarContent>
                         </MenubarMenu>
                     </div>
